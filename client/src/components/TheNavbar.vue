81774d90b5573401f2f35d7ef5306db1c562f78a
Isaac Folmsbee <if@sent.com>
2021-02-04 20:45:46 -0500

    placeholder files and mobile navbar

---
+++ client/src/components/TheNavbar.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="bg-gray-800 w-screen h-16 flex items-center justify-between">
+	<h1 class="ml-3 font-bold text-4xl"><span class="text-green-500">F</span><span class="text-blue-500">M</span><span class="text-gray-500 font-medium text-xl"> Records</span></h1>
+	<svg class="h-10 w-10 mr-3 text-gray-500 fill-current" viewBox="0 0 24 24"><path d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2z"></path></svg>
+</div>
+</template>
